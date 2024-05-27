3e18b3d09e3a2f709d2f276a70da4b17e578f333
Lalluss <123368967+Lalluss@users.noreply.github.com>
2024-05-27 22:13:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ api_key = os.environ.get('tmdbApiKey')
 PORT = int(os.environ.get('PORT', '8443'))
 logger = logging.getLogger(__name__)
 
-TOKEN = os.environ.get('TOKEN')
+TOKEN = os.environ.get('TOKEN', "7308805741:AAH_OzX5RGUmsNOugNQ3SIfSZrITSCqJAeU")
 name = ''
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
