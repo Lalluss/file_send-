1250e1d3204b705b7fff6889092eea9c192f46e7
SᴛᴇʟʟᴀʀLᴀʙs <158211667+Daneil-k@users.noreply.github.com>
2024-05-28 10:13:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from telegram.ext import (
 )
 
 api_key = os.environ.get('tmdbApiKey', "1988873da44475ed5d56e0465a63e42f")
-PORT = int(os.environ.get('PORT', '8080'))
+PORT = int(os.environ.get('PORT', '8443'))
 logger = logging.getLogger(__name__)
 
 TOKEN = os.environ.get('TOKEN', "7308805741:AAH_OzX5RGUmsNOugNQ3SIfSZrITSCqJAeU")
