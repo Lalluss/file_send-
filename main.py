be52391dedd1c7f2ae6d2b344e46758a371dbef9
Lalluss <123368967+Lalluss@users.noreply.github.com>
2024-05-30 13:16:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -342,9 +342,8 @@ async def search_item(update: Update, message) -> None:
         else:
             # save the results for easy pagination
             ResultsCache[update.effective_message.chat_id] = {
-                {unique_id_str: [{f'{index + 1}': {key: movie_item[key] for key in req_cols}
-                                for index, movie_item in enumerate(req)}]
-            }
+                unique_id_str: [{f'{index + 1}': {key: movie_item[key] for key in req_cols}
+                                for index, movie_item in enumerate(req)}]}
 
         else:
             await update.callback_query.message.reply_text(
